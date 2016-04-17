d866e3a2cca0f09eb3932b0b18f8cc6e3d8eaf2e
[A <scullingnull+lisp@gmail.com>
2016-04-17 10:24:07 -0700

    Whole point of exercise is have to track state outside

--- exercises/ex1/ex1.js
+++ exercises/ex1/ex1.js
@@ -23,9 +23,32 @@ function output(text) {
 function getFile(file) {
 	fakeAjax(file,function(text){
 		// what do we do here?
+		processResponse(file, text);
 	});
 }
 
+var responses = {};
+
+function processResponse(url, text) {
+	if (!(url in responses)) {
+		responses[url] = text;
+	}
+
+// other way is to save list as you request, rather than hard coding
+	var files = ["file1", "file2", "file3"];
+
+	for (var i = 0; i < files.length; i++) { //outer loop lets you print as you get response back
+		if (files[i] in responses) { // takes care of randomized response order
+			if(responses[files[i]] !== null){ //makes sure does not reprint if already reprinted
+				output(responses[files[i]]);
+				responses[files[i]] = null;
+			}
+		} else {
+			return false;
+		}
+	}
+	output("Complete!");
+}
 // request all files at once in "parallel"
 getFile("file1");
 getFile("file2");
