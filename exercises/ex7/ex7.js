cba3b64cad89e91078fb5a5162c77fd50391bf2d
[A <scullingnull+lisp@gmail.com>
2016-04-17 16:22:24 -0700

    Generator + promises

--- exercises/ex7/ex7.js
+++ exercises/ex7/ex7.js
@@ -33,3 +33,39 @@ function getFile(file) {
 // is done.
 
 // ???
+ASQ()
+.runner(function*() {
+	// problem with below, not doing it in parallell
+	// var text1 = yield getFile("file1");
+	// output(text1);
+	// var text2 = yield getFile("file2");
+	// output(text2);
+	// var text3 = yield getFile("file3");
+	// output(text3);
+
+	var s1 = getFile("file1");
+	var s2 = getFile("file2");
+	var s3 = getFile("file3");
+
+	// var text1 = yield s1;
+	// output(text1);//subsitute with below
+
+	// var text2 = yield s2;
+	// output(text2);
+	//
+	// var text3 = yield s3;
+	// output(text3);
+
+	output(yield s1);
+	output(yield s2);
+	output(yield s3);
+
+	output("Complete!");
+	// no compromise here (except with using yield keyword) - reads synchronous
+
+	// the beauty of this is why Kyle would never go back to using promise chains
+
+	// for generator doesn't matter where asterix goes - just has to show up between function and parentheses
+
+	// async is to bring in all async libraries into one library
+});
