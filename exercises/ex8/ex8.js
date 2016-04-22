5e2f04a77657a8659ad22e1f53367dfa6bde9bda
[A <scullingnull+lisp@gmail.com>
2016-04-21 19:02:02 -0700

    Last checkin - not verified

--- exercises/ex8/ex8.js
+++ exercises/ex8/ex8.js
@@ -1,10 +1,41 @@
-$(document).ready(function(){
-	var $btn = $("#btn"),
-		$list = $("#list");
+$(document).ready(function() {
+    var $btn = $("#btn"),
+        $list = $("#list");
 
-	$btn.click(function(evt){
-		// TODO
+    var clicks = ASQ.react.of();
+
+    $btn.click(function(evt) {
+        // TODO
+				clicks.push(evt);
+    });
+
+		var throttled = debounce(clicks);
+
+
+    // TODO: setup sampled sequence, populate $list
+		clicks
+		.val(function(evt){
+			// console.log("clicked!");
+			$list.append("<p>Clicked!</p>");
+		});
+});
+
+function debounce(clicks) {
+
+	var last;
+
+	clicks.val(function evt() {
+			last = evt;
 	});
 
-	// TODO: setup sampled sequence, populate $list
-});
+	setInterval(function(){
+			if (last) {
+					throttled.push(last);
+					last = null;
+			}
+	},1000);
+
+	var throttled = ASQ.react.of();
+
+	return throttled;
+}
